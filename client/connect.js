1eb37fb5f908e3f78069411630af30cd7dfb75e6
Tim Bartsch <timb.stud@gmail.com>
2011-01-13 22:42:11 +0100

    json korrigiert

--- client/connect.js
+++ client/connect.js
@@ -1,11 +1,14 @@
 var session = 0;
 var url = "url:8124";
 function init(){
-	$.post(url, "newGame:1", newGameHandler, "json");
+	var msg = JSON.stringify({
+    	"newGame": "1"
+  	});
+	$.post(url, msg, newGameHandler, "json");
 }
 
 function newGameHandler(data){
-	$("#status").html("new Game: " + data);
+	$("#status").html("new Game" + data);
 	session = data;
 	$.post(url, "poll:1", pollHandler, "json");
 }
