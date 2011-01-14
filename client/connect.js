a733b5d535fa8a25faed1e494b5ef71c1e058db0
Tim Bartsch <timb.stud@gmail.com>
2011-01-14 20:56:40 +0100

    json.type nun mit switch case

--- client/connect.js
+++ client/connect.js
@@ -1,14 +1,14 @@
 var sid = 0;
 var url = "url:8124";
 function init(){
-	var msg = JSON.stringify({"newGame": "1"});
+	var msg = JSON.stringify({"type": "newGame"});
 	$.post(url, msg, newGameHandler, "json");
 }
 
 function newGameHandler(data){
 	$("#status").html("new Game" + data);
 	sid = data;
-	var msg = JSON.stringify({	"poll": "1",
+	var msg = JSON.stringify({	"type": "poll",
 								"sid": sid});
 	$.post(url, msg, pollHandler, "json");
 }
--- server/sessions.js
+++ server/sessions.js
@@ -25,26 +25,34 @@ http.createServer(function(req, res){
 	req.on("data", function(chunk){
 		var json = JSON.parse(chunk);
 		console.log(json);
-		if(json.newGame){
-			var session = new Session();
-			sessions.push(session);
-			res.writeHead(200, {
-            	'Content-Type': 'text/plain',
-            	'Access-Control-Allow-Origin' : '*'
-        	});
-			res.end("" + session.sid);
-		}else{
-			if(json.poll){
-				session = getSession(json.sid);
-				switch(json.player){
-					case "1":
-						session.player1Res = res;
-						break;
-					case "2":
-						session.player2Res = res;
-						break;
+		switch(json.type){
+			case "newGame":
+					var session = new Session();
+					sessions.push(session);
+					res.writeHead(200, {
+           			 	'Content-Type': 'text/plain',
+         			   	'Access-Control-Allow-Origin' : '*'
+        			});
+					res.end("" + session.sid);
+					break;
+			case "poll":
+					var session = getSession(json.sid);
+						switch(json.player){
+							case "1":
+								session.player1Res = res;
+								break;
+							case "2":
+								session.player2Res = res;
+								break;
+						}
 				}
-			}
+					break;
+			case "send":
+					
+					break;
+			default:
+					console.log("Wrong query Type:" + json.type);
+					break;
 		}
 	});	
 }).listen(8124);
