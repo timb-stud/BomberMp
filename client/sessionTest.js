0ba8051bbbf8aff2385edd8300061986f0e0064d
Tim Bartsch <timb.stud@gmail.com>
2011-01-18 22:14:58 +0100

    url join eingefegt und sessionTests umbenannt

---
+++ client/sessionTest.js
@@ -0,0 +1,44 @@
+var url = "http://localhost:8124",
+	session = null;
+
+function init(){
+	session = new Session(url, msgHandler);
+	var sid = getSidFromUrl();
+	if(sid){
+		session.join(sid);
+	}
+}
+
+function getSidFromUrl(){
+	var params = window.location.search,
+		expr = /sid\=(\d{4})/,
+		sid = 0;
+		if(expr(params)){
+			sid = expr(params)[1];
+		}
+		console.log("sidFromUrl", sid);
+		return sid;
+}
+
+function msgHandler(msg){
+	console.log("msgHandler", msg);
+}
+
+
+function newSession(){
+	session.create();
+	console.log(window.location.href + "?sid=" + session.getSid());
+}
+
+function joinSession(){
+	var sid = $("#sidBox").attr("value");
+	session.join(sid);
+}
+
+
+function send(){
+	var msg = $("#msgBox").attr("value");
+	session.send(msg);
+}
+
+$(document).ready(init);
