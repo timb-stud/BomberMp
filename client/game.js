bfcdc0f65642205c9dad06117e4c4535ad78337a
Tim Bartsch <timb.stud@gmail.com>
2011-01-30 22:14:05 +0100

    collision Detection korrigiert

--- client/game.js
+++ client/game.js
@@ -1,9 +1,11 @@
 var w;
 var h;
-var ctx;
-var player;
-var pg;
-var keyPressed = {
+var ctx,
+	w,
+	h,
+	player,
+	walls = [],
+	keyPressed = {
     up: false,
     down: false,
     left: false,
@@ -11,57 +13,50 @@ var keyPressed = {
 };
 
 function onKeyDown(evt) {
-    if (evt.keyCode == 38) keyPressed.up = true;
-    if (evt.keyCode == 40) keyPressed.down = true;
-    if (evt.keyCode == 37) keyPressed.left = true;
-    if (evt.keyCode == 39) keyPressed.right = true;
+    if (evt.keyCode == 38){ keyPressed.up = true;}
+    if (evt.keyCode == 40){ keyPressed.down = true;}
+    if (evt.keyCode == 37){ keyPressed.left = true;}
+    if (evt.keyCode == 39){ keyPressed.right = true;}
 }
 
 function onKeyUp(evt) {
-    if (evt.keyCode == 38) keyPressed.up = false;
-    if (evt.keyCode == 40) keyPressed.down = false;
-    if (evt.keyCode == 37) keyPressed.left = false;
-    if (evt.keyCode == 39) keyPressed.right = false;
-    if (evt.keyCode == 32) player.dropBomb();
+    if (evt.keyCode == 38){ keyPressed.up = false;}
+    if (evt.keyCode == 40){ keyPressed.down = false;}
+    if (evt.keyCode == 37){ keyPressed.left = false;}
+    if (evt.keyCode == 39){ keyPressed.right = false;}
+    if (evt.keyCode == 32){ player.dropBomb();}
 }
 
 $(document).keydown(onKeyDown);
 $(document).keyup(onKeyUp);
+
+function mainLoop(){
+    ctx.clearRect(0, 0, w, h);
+    for(i=0; i< walls.length; i++){
+		walls[i].draw(ctx);
+    }
+    player.draw(ctx);
+    $("#status").html("keyPressed.up: " + keyPressed.up
+        + "<br> keyPressed.down: " + keyPressed.down
+        + "<br> keyPressed.left: " + keyPressed.left
+        + "<br> keyPressed.right: " + keyPressed.right);
+    if(keyPressed.up){ player.moveUp();}
+    if(keyPressed.down){ player.moveDown();}
+    if(keyPressed.left){ player.moveLeft();}
+    if(keyPressed.right){ player.moveRight();}
+}
 
 function init(){
     var canvas = $("#gameCanvas")[0];
     w = canvas.width;
     h = canvas.height;
     ctx = canvas.getContext("2d");
-
-    pg = new Playground(w/20, h/20);
-    pg.addWall(1, 1);
-    pg.addWall(1, 2);
-    pg.addWall(1, 3);
-    pg.addWall(1, 4);
-    pg.addWall(1, 5);
-    pg.addWall(1, 6);
-    pg.addWall(2, 1);
-    pg.addWall(8, 7);
-    pg.addWall(8, 8);
-    pg.addWall(7, 8);
-    player = new Player(pg, 3, 3);
+    walls.push(new Wall(50, 50));
+    walls.push(new Wall(50, 70));
+    walls.push(new Wall(50, 90));
+    walls.push(new Wall(70, 70));
+    player = new Player(3, 3, walls);
     setInterval(mainLoop, 30);
-}
-
-function mainLoop(){
-    ctx.clearRect(0, 0, w, h);
-    pg.draw(ctx);
-    player.draw(ctx);
-    player.update();
-    $("#status").html("keyPressed.up: " + keyPressed.up
-        + "<br> keyPressed.down: " + keyPressed.down
-        + "<br> keyPressed.left: " + keyPressed.left
-        + "<br> keyPressed.right: " + keyPressed.right);
-    if(keyPressed.up) player.moveUp();
-    if(keyPressed.down) player.moveDown();
-    if(keyPressed.left) player.moveLeft();
-    if(keyPressed.right) player.moveRight();
 }
 
 $(document).ready(init);
--- client/gameObjects.js
+++ client/gameObjects.js
@@ -11,16 +11,28 @@ var Wall = function(x,y){
 	this.y = y;
 	this.w = 20;
 	this.h = 20;
+	this.isLeftOf = function(go){
+		return this.x + this.w < go.x;
+	};
+		this.isAbove = function(go){
+		return this.y + this.h < go.y;
+	};
 	this.draw = function(ctx){
-		drawRect(ctx, "#8B4513", this.x * this.w, this.y * this.h, this.w, this.h);
+		drawRect(ctx, "#8B4513", this.x, this.y, this.w, this.h);
 	};
 };
 
 var Bomb = function(x, y, radius){
-	var w = 20;
-	var h = 20;
+	this.w = 20;
+	this.h = 20;
 	this.x = x;
 	this.y = y;
+	this.isLeftOf = function(go){
+		return this.x + this.w < go.x;
+	};
+		this.isAbove = function(go){
+		return this.y + this.h < go.y;
+	};
 	this.explode = function(){
 		for(i=1; i <= radius;i++){
 			playground.remove(x + i, y);
@@ -38,9 +50,9 @@ var Bomb = function(x, y, radius){
 var Player = function(x, y, walls){
 	this.x = x;
 	this.y = y;
-	var w = 20,
-		h = 20,
-		bomb = null,
+	this.w = 20;
+	this.h = 20;
+	var bomb = null,
 		bombRadius = 2,
 		bombTimer = 2000,
 		steps = 2 ;
@@ -48,8 +60,8 @@ var Player = function(x, y, walls){
 	this.isLeftOf = function(go){
 		return this.x + this.w < go.x;
 	};
-	this.isRightOf = function(go){
-		return this.y < go.y + go.h;
+	this.isAbove = function(go){
+		return this.y + this.h < go.y;
 	};
 	this.touches = function(go){
 		if(this.isLeftOf(go)){	return false;}
@@ -65,6 +77,7 @@ var Player = function(x, y, walls){
 		this.y = y;
 		for(i=0; i < walls.length; i++){
 			if(this.touches(walls[i])){
+				console.log("touches");
 				this.x = xTmp;
 				this.y = yTmp;
 				return;
@@ -72,16 +85,16 @@ var Player = function(x, y, walls){
 		}
 	};
 	this.moveUp = function(){
-		this.move(this.x, this.y - 1);
+		this.moveTo(this.x, this.y - 1);
 	};
 	this.moveDown = function(){
-		this.move(this.x, this.y + 1);
+		this.moveTo(this.x, this.y + 1);
 	};
 	this.moveLeft = function(){
-		this.move(this.x - 1, this.y);
+		this.moveTo(this.x - 1, this.y);
 	};
 	this.moveRight = function(){
-		this.move(this.x + 1, this.y);
+		this.moveTo(this.x + 1, this.y);
 	};
 	this.dropBomb = function(){
 		bomb = new Bomb(playground, this.x, this.y, bombRadius);
@@ -92,7 +105,7 @@ var Player = function(x, y, walls){
 		bomb = null;
 	};
 	this.draw = function(ctx){
-		drawRect(ctx, "#97FFFF", rx, ry, w, h);
+		drawRect(ctx, "#97FFFF", this.x, this.y, this.w, this.h);
 		if(bomb){
 			bomb.draw(ctx);
 		}
